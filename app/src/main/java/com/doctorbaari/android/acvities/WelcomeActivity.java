bdff6cdbbaae6a95fb014c2ed3bf7202ae730eed
Nowfel Mashnoor <nmmashnoor@gmail.com>
2017-11-06 17:35:36 +0600

    Inital Commit

---
+++ app/src/main/java/com/doctorbaari/android/acvities/WelcomeActivity.java
@@ -0,0 +1,22 @@
+package com.doctorbaari.android.acvities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.doctorbaari.android.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void goMbbs(View v) {
+        startActivity(new Intent(WelcomeActivity.this, RegistrationActivity.class));
+
+    }
+}
